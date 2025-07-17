1bdee61f1880af3d0875a1291a3a45c0d04ea793
rweilwww <github@shaw-weil.com>
2025-07-17 15:24:10 -0400

    from indesign 2025-07-17 19:03

--- gitpush.bat
+++ gitpush.bat
@@ -1,14 +1,9 @@
 
 cd /inetpub/wwwroot/w/stavich
-git init
-            git config --global --add safe.directory C:/inetpub/wwwroot/w/stavich
-   git remote add origin https://github.com/ShawWeil/web-stavich.git
-   git remote -v
 
-        
 
 git add .
-git commit -m "from indesign 2025-06-28 20:55" --no-edit
+git commit -m "from indesign 2025-07-17 19:03" --no-edit
 git push --set-upstream origin main
 
 rem wait for one minute to allow git hub to get its act together
