15154408618f1216805fb6d01b1c8b86a689bb82
rweilwww <github@shaw-weil.com>
2025-07-22 12:49:43 -0400

    from indesign 2025-07-22 16:48

--- gitpush.bat
+++ gitpush.bat
@@ -3,7 +3,7 @@ cd /inetpub/wwwroot/w/stavich
 
 
 git add .
-git commit -m "from indesign 2025-07-17 19:40" --no-edit
+git commit -m "from indesign 2025-07-22 16:48" --no-edit
 git push --force --set-upstream origin main
 
 rem wait for one minute to allow git hub to get its act together
